e5c83d9f2400d534da62a825997a86eaf4fde486
orbuluh <scuendless@gmail.com>
2022-12-23 10:50:16 +0800

    modify solution comment for 309. Best Time to Buy and Sell Stock with Cooldown

--- problem_sets/dp/dp_state_transition/buy_sell_stock_with_cooldown.h
+++ problem_sets/dp/dp_state_transition/buy_sell_stock_with_cooldown.h
@@ -1,14 +1,33 @@
 /*
-b, s, c: profit of last operation being buy/sell/cool down respectively
+
+Note that the problem description actually have 2 hidden assumptions:
+
+1. you have unlimited amount of inventory: so you can sell more than what you've
+   bought
+2. you have to buy before sell, which is the other aspect of the problem
+   description:
+   "You may not engage in multiple transactions simultaneously (i.e., you must
+   sell the stock before you buy again).""
+
 The trick is including buy price p as -p into the profit, then you don't
 need to keep track of the buy price for each state, you just need to
 focus on maximize the profit.
 
-b[i] = max(c[i - 1] - p,              # previously chill, and buy now
-           b[i - 1])                  # do nothing, same as last buy
-s[i] = max(b[i - 1] + p,              # sell at i, from last buy
-          s[i - 1])                   # do nothing, same as last sell
-c[i] = max(s[i - 1], c[i - 1])        # sell previously and chill or just chill
+B = buy[i] means max profit before day i "for any sequence end with buy".
+S = sell[i] means max profit before day i "for any sequence end with sell".
+C = cd[i] means max profit before day i "for any sequence end with cooldown"
+
+Note the state represents "any sequence end with X",
+NOT that "at day i, it must be X"
+
+So transition function is like
+
+B' = max(C - p, B) ==> previous state C then buy, or doing nothing at i
+                       and inherit the previous sequence that end with B
+S' = max(B + p, S) ==> previous state C then sell, or doing nothing at i
+                       and inherit the previous sequence that end with S
+C' = max(S, C)     ==> previous state S then cd, or doing nothing at i
+                       and inherit the previous sequence that end with C
 */
 class Solution {
  public:
@@ -17,50 +36,11 @@ class Solution {
     std::array<int, 3> bsc = {INT_MIN, INT_MIN, 0};
     auto nxtBsc = bsc;
     for (auto p : prices) {
-      nxtBsc[B] = std::max(bsc[B], bsc[C] - p);
-      nxtBsc[S] = std::max(bsc[S], bsc[B] + p);
-      nxtBsc[C] = std::max(bsc[C], bsc[S]);
+      nxtBsc[B] = std::max(bsc[C] - p, bsc[B]);
+      nxtBsc[S] = std::max(bsc[B] + p, bsc[S]);
+      nxtBsc[C] = std::max(bsc[S], bsc[C]);
       std::swap(bsc, nxtBsc);
     }
     return std::max(bsc[1], bsc[2]);
   }
-
-
-/* The other way that will also work:
-
-dp[i] = max profit when doing X at i
-
-buy at i can either from previous buy (no buy at i) or
-previous cooldown - price(buy at i)
-- B[i] = std::max(C[i - 1] - prices[i], B[i - 1])
-
-sell at i can only from buy at i - 1
-- S[i] = B[i - 1] + prices[i]
-
-cooldown at i can either be from sell at i - 1
-or cooldown at i - 1
-- C[i] = std::max(S[i - 1], C[i - 1])
-
-initially:
-B[0] = -prices[0]
-S[0] = 0
-C[0] = 0
-
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-      const auto sz = prices.size();
-      // 0: B, 1: S, 2: Cooldown
-      std::array<int, 3> bsc = { -prices[0], 0, 0 };
-      std::array<int, 3> nxtBsc = {};
-      for (int i = 1; i < sz; ++i) {
-        nxtBsc[0] = std::max(bsc[2] - prices[i], bsc[0]);
-        nxtBsc[1] = bsc[0] + prices[i];
-        nxtBsc[2] = std::max(bsc[1], bsc[2]);
-        std::swap(bsc, nxtBsc);
-      }
-      return std::max(bsc[1], bsc[2]);
-    }
 };
-
-*/
