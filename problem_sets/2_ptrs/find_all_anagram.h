7f4904b34bda6df244ee5816bb59607ae615c9be
orbuluh <scuendless@gmail.com>
2023-02-05 08:39:58 +0800

    438. Find All Anagrams in a String - better explanation/implementation

--- problem_sets/2_ptrs/find_all_anagram.h
+++ problem_sets/2_ptrs/find_all_anagram.h
@@ -1,50 +1,33 @@
 /*
-Idea: using the template of counting substring ...
+Idea: same as 567. The key thing is keeping a valid window of (sub)anagram of p
+in s.
 
-keep a valid window of finding an anagram, output it and invalidate it
-then finding next one, using 2 pointers.
-
-The caveat here is that you need to maintain the window size as anagram is with
-the same size. So overall it might be easier to do a standard sliding window
-where you maintain a counter of p, and a counter of sliding window. And just
-compare if 2 counters are the same while you iterate...
+Use 2 pointers to maintain such invariance in the window. How? The counter of p
+can't be reduced with a value less than 0. When it happens, then we need to
+shrink the left (e.g. pop'ing out value in the window) till the invariant holds
+again. The left pointer is basically the start of the current window.
 */
 class Solution {
  public:
   vector<int> findAnagrams(string s, string p) {
-    int l = 0;
-    int r = 0;
-    int unmatch = p.size();
-    auto idx = [](char c) { return c - 'a'; };
     std::vector<int> counter(26, 0);
     for (char c : p) {
-      counter[idx(c)]++;
+      counter[c - 'a']++;
     }
-    std::vector<int> res;
-    while (r < s.size()) {
-      // r will become non-inclusive
-      if (counter[idx(s[r++])]-- > 0) {
-        unmatch--;
-      }
-      while (l < r && counter[idx(s[l])] < 0) {
-        counter[idx(s[l++])]++;  // remove unrelated chars
-      }
-      while (r - l > p.size()) {
-        // shrink l s.t. the window size is right
-        // along the way it could invalid the window again
-        // consider: s = aabc, p = abc
-        // it would have become, l = 0, r = 4, unmatched = 0
-        // you need to make l = 1, so that r - l is as p.size()
-        if (++counter[idx(s[l++])] > 0) {
-          unmatch++;
+    const int sz = s.size();
+    std::vector<int> result;
+    for (int l = 0, r = 0; r < sz; ++r) {
+      if (--counter[s[r] - 'a'] < 0) {// invariant breaks
+        while (counter[s[r] - 'a'] < 0) { // recover the invariant
+          counter[s[l++] - 'a']++; // through shrinking left
         }
       }
-      if (unmatch == 0) {
-        res.push_back(l);
-        counter[idx(s[l++])]++;  // make it invalid again
-        unmatch++;
+      // window is always valid. And if size is  equal to p.size(), it must
+      // be a full match!
+      if (r - l + 1 == p.size()) {
+        result.push_back(l);
       }
     }
-    return res;
+    return result;
   }
 };
