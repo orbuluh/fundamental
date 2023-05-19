912976d757415ede0daf848526f2429f08b6b373
orbuluh <scuendless@gmail.com>
2023-05-19 15:22:40 +0800

    update solution for 785. Is Graph Bipartite?

--- problem_sets/graph/is_graph_bipartite.h
+++ problem_sets/graph/is_graph_bipartite.h
@@ -1,59 +1,43 @@
 class Solution {
-  /*
-  Bipartite:
-  - "every edge in the graph connects a node in set A and a node in set B."
+/*
 
-  This implies:
-  - "no edges between nodes in the same set in a bipartite graph"
+A graph can be broken down to several connected component.
 
-  Say we always do this in a DFS way, where whenever we meet a new node, we
-  mark it with one set and then do a DFS way to make sure all its neighbor
-  is in another set.
+For each of the connected component, if you do DFS on it, then each adjacent
+nodes must not have same color. (As Bipartite requires.)
 
-  The question is - can we randomly assign it with one color?
+So we simply start from one node and DFS it, while marking a node a color.
+Once DFS returned, then the connected component for the node is done.
 
-  The answer is yes. Why? If we meet a new node that hasn't been seen before,
-  it means all the nodes we've seen cannot make an edge to this node. So they
-  are all unrelated to it. (Note that the graph is undirected, so it implies
-  either new node has no edge to previous node and vise-versa.)
+We just find the next non-visited node, which must be on a different connected
+component. Then we do the same, DFS this component and see if any node is
+breaking the requirement of bipartite.
 
-  So because we are doing DFS, all the connected nodes should have been
-  checked when the recursion returns. So whenever we are meeting with a node
-  without any assignment, we must be starting a new connected partial graph
-  than what was previously checked. So we can always randomly assign a set to
-  this new node!
-  */
+*/
+
  public:
-  enum class Set {
-    None,  // not yet assign
-    A,
-    B,
-  };
-  std::vector<Set> sets;
+  std::vector<int> nodeColor;
   bool isBipartite(vector<vector<int>>& graph) {
-    sets = std::vector<Set>(graph.size(), Set::None);
-    for (int i = 0; i < graph.size(); ++i) {
-      // if sets[i] != None, we've checked its corresponding connected
-      // (partial) graph. So we will just continue.
-      // Otherwise we assume the new node to be in one set, and exam
-      // its parent in a DFS fashion, to make sure all nodes in the
-      // connected (partial) graph with node i are valid.
-      if (sets[i] == Set::None && !dfsValidate(graph, i, Set::A)) {
+    const int sz = graph.size();
+    nodeColor = std::vector<int>(sz, 0);
+    for (int i = 0; i < sz; ++i) {
+      // randomly assign a color for the new component
+      if (!nodeColor[i] && !dfsValidate(graph, i, 1)) {
         return false;
       }
     }
     return true;
   }
 
-  bool dfsValidate(vector<vector<int>>& graph, int idx, Set assignSet) {
-    if (sets[idx] != Set::None) {
-      return assignSet == sets[idx];
+  bool dfsValidate(vector<vector<int>>& graph, int idx, int assignedColor) {
+    if (nodeColor[idx]) {  // have been assigned for a color
+      return assignedColor == nodeColor[idx];
     }
+    const auto adjColor = -1 * assignedColor;
+    nodeColor[idx] = assignedColor;
     const auto& edges = graph[idx];
-    const auto otherSet = assignSet == Set::A ? Set::B : Set::A;
-    sets[idx] = assignSet;
     for (auto neighbor : edges) {
-      if (!dfsValidate(graph, neighbor, otherSet)) {
+      if (!dfsValidate(graph, neighbor, adjColor)) {
         return false;
       }
     }
