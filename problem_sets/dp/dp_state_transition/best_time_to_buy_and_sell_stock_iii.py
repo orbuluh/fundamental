cdcc6dcdaf1caf280ea84f37484bd6233598bebd
orbuluh <scuendless@gmail.com>
2024-06-17 23:11:15 +0900

    123. Best Time to Buy and Sell Stock III ... add some description

--- problem_sets/dp/dp_state_transition/best_time_to_buy_and_sell_stock_iii.py
+++ problem_sets/dp/dp_state_transition/best_time_to_buy_and_sell_stock_iii.py
@@ -13,13 +13,13 @@ class Solution:
 
         buy2 and sell2 then track the best outcomes after potentially
         completing a second transaction (buying again and then selling again).
+        e.g.  sell2 represents the maximum profit achievable after completing
+        ___at most___ two transactions
 
         the first time you buy, your total asset will becomes -p1
         the first time you sell, your total asset becomes p2 + buy1
         the second time you buy, your total asset becomes sell1 -p3
         the second time you sell, your total asset becomes p4 + buy2
-
-
         """
         # set buy* to min because it's expected that the first time you buy
         # something, your total asset becomes negative. And you just want to
@@ -36,5 +36,54 @@ class Solution:
             sell1 = max(sell1, p + buy1)
             buy2 = max(buy2, sell1 - p)
             sell2 = max(sell2, p + buy2)
-        # why not return max(sell2, sell1)? can't really rationale???
+        # why not return max(sell2, sell1)? because sell2 means the max profit
+        # you will get considering at max 2 transactions, which includes the
+        # case that there is only 1 transaction!
         return sell2
+
+
+class SolutionDP2:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        Say dp[k, j] is max total asset with at most k transactions on j-th day.
+        if we don't trade then the total asset is same as previous day:
+            dp[k, j-1]       ... case 1
+
+        if we bought the share on i-th day where i=[0..j-1]
+        and sell the share on j-th day make total asset:
+            prices[j] - prices[i] + dp[k-1, i-1]   ... case 2
+
+        where dp[k-1, i-1] is the total asset you hold before day i, and with
+        at most k - 1 transactions.
+
+
+        So the recursion will be:
+        max(case1, case2) = max(
+            dp[k, j-1],
+            max(prices[j] - prices[i] + dp[k-1, i-1] where i is 0 to j - 1
+        )
+        so while you iterating j (the later index), in case 2, you know that
+        you want to keep track of this value, so you don't need to loop the
+        smaller index i where i < j again, this can be consider the
+        max_total_asset_seen = -prices[i] + dp[k - 1, i - 1]
+
+        check https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/solutions/135704/detail-explanation-of-dp-solution
+        this solution, through simplify to 1D dp array, and rename variable,
+        will become first solution ... (mind blow ...)
+        The solution is not using max_total_asset_seen but the reverse
+        of min_diff_total_asset? but not as straightforward?
+        and why init as -prices[0] also not clear yet...
+        """
+
+        if not prices:
+            return
+        dp = [[0] * len(prices) for _ in range(3)]
+        max_total_asset_seen = [-prices[0]] * 3  # why? need to think ...
+        n = len(prices)
+        for j in range(1, n):
+            for k in [1, 2]:
+                max_total_asset_seen[k] = max(
+                    max_total_asset_seen[k], -prices[j] + dp[k - 1][j - 1]
+                )
+                dp[k][j] = max(dp[k][j - 1], prices[j] + max_total_asset_seen[k])
+        return dp[2][-1]
