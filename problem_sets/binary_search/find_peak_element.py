00d505d902b8763e79773568074ecad64d2fc262
orbuluh <scuendless@gmail.com>
2024-07-11 21:47:48 +0900

    162. Find Peak Element

--- problem_sets/binary_search/find_peak_element.py
+++ problem_sets/binary_search/find_peak_element.py
@@ -1,21 +1,14 @@
 class Solution:
-    """
-    You are either on the peak, or the road to the peak ...
-    peak must exist, and around the peak, it must be increasing on the left
-    and decreasing on the right. E.g. if you know the left element to you
-    is greater , you can say either that is peak or there must be a greater
-    element than this on the left side. likewise logic for element right to you.
-    if the array is flat, then eventually converging to either side is answer
-    """
-
     def findPeakElement(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
         while left < right:
             mid = left + (right - left) // 2
-            if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
-                return mid
-            elif nums[mid] < nums[mid - 1]:
+            if nums[mid] > nums[mid + 1]:
+                # If mid element is greater than its right neighbor,
+                # then the peak lies on the left side (including mid)
                 right = mid
             else:
+                # If mid element is smaller than or equal to its right neighbor,
+                # then the peak lies on the right side (excluding mid)
                 left = mid + 1
         return left
